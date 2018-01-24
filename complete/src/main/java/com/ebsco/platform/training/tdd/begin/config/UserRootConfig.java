3ccdbd73b2661ea5c046990b51a0bfb27cde2375
Sanjay Bhupathiraju <sbhupathiraju@ebsco.com>
2018-01-23 23:08:41 -0500

    rootconfig's default profile added

--- complete/src/main/java/com/ebsco/platform/training/tdd/begin/config/UserRootConfig.java
+++ complete/src/main/java/com/ebsco/platform/training/tdd/begin/config/UserRootConfig.java
@@ -3,9 +3,11 @@ package com.ebsco.platform.training.tdd.begin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Import({UserTxMgmtConfig.class, UserHALConfig.class})
+@Profile("default")
 public class UserRootConfig {
 
 }
